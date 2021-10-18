2f7b76bbdc7bee9ff8c3d2231e46c7462f3bd446
Oliver Hall <olly.hall97@gmail.com>
2021-10-18 18:48:40 +0100

    Sequence created and user sequence stored. Breaks after one round.

--- script.js
+++ script.js
@@ -1,34 +1,82 @@
-"strict";
+'strict';
 
 // Elements
-const tileOne = document.querySelector(".tile--red");
-const tileTwo = document.querySelector(".tile--blue");
-const tileThree = document.querySelector(".tile--yellow");
-const tileFour = document.querySelector(".tile--purple");
-const tileFive = document.querySelector(".tile--pink");
-const tileSix = document.querySelector(".tile--brown");
+const tileZero = document.querySelector('.tile--red');
+const tileOne = document.querySelector('.tile--blue');
+const tileTwo = document.querySelector('.tile--yellow');
+const tileThree = document.querySelector('.tile--purple');
+const tileFour = document.querySelector('.tile--pink');
+const tileFive = document.querySelector('.tile--brown');
 
-const command = document.querySelector(".command--display");
+const command = document.querySelector('.command--display');
 
 // Variables
-let sequence = "";
+let sequence = [];
+let userSeq = [];
+const tileNumberToColour = ['Red', 'Blue', 'Yellow', 'Purple', 'Pink', 'Brown'];
 
-// Functions
-const nextSeqEl = () => Math.floor(Math.random() * 6 + 1);
-sequence += nextSeqEl();
+// Functions \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//////////////////////////////////////////////////
+const nextSeqEl = () => sequence.push(Math.floor(Math.random() * 6));
+nextSeqEl();
 
-const init = function () {
-  command.textContent =
-    "Copy the sequence displayed here using the buttons below.";
+// Interval timer for showing the colour sequence.
+const seqDisplay = function () {
+  let count = 0;
+  const timer = setInterval(function () {
+    if (count === sequence.length) {
+      command.textContent = 'Enter the sequence using the buttons';
+      clearInterval(timer);
+    }
+    command.textContent = tileNumberToColour[Number(sequence[count])];
+    count++;
+  }, 1000);
 };
+seqDisplay();
 
-const sequenceDisplay = function () {
-  for (const el of sequence) {
-    command.textContent = el;
-    // needs timer
+// Check sequeunce and if complete starts next round or restarts game when wrong.
+const seqCheck = function () {
+  if (
+    userSeq.length === sequence.length &&
+    userSeq.join('') === sequence.join('')
+  ) {
+    command.textContent = 'Sequence correct!';
+    nextSeqEl();
+    setTimeout(seqDisplay, 1000);
   }
+  if (userSeq.length === sequence.length && userSeq !== sequence)
+    command.textContent = 'Incorrect, try again';
+  sequence = [];
+  userSeq = [];
 };
 
-init();
+// Event handlers
+tileZero.addEventListener('click', function () {
+  userSeq.push(0);
+  seqCheck();
+});
 
-sequenceDisplay();
+tileOne.addEventListener('click', function () {
+  userSeq.push(1);
+  seqCheck();
+});
+
+tileTwo.addEventListener('click', function () {
+  userSeq.push(2);
+  seqCheck();
+});
+
+tileThree.addEventListener('click', function () {
+  userSeq.push(3);
+  seqCheck();
+});
+
+tileFour.addEventListener('click', function () {
+  userSeq.push(4);
+  seqCheck();
+});
+
+tileFive.addEventListener('click', function () {
+  userSeq.push(5);
+  seqCheck();
+});
